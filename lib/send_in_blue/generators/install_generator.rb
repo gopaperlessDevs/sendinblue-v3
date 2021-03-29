291388bfe39b3308e3518317b066de5549efa037
Ethaning <ethanjcarter@hotmail.co.uk>
2021-03-29 16:16:12 +0100

    Fix bug with generator

--- lib/send_in_blue/generators/install_generator.rb
+++ lib/send_in_blue/generators/install_generator.rb
@@ -8,27 +8,28 @@ module SendInBlue
       desc "Setup Send In Blue integration"
       class_option :orm, required: true
 
-    def self.source_root
-      @source_root ||= File.join(File.dirname(__FILE__), 'templates')
-    end
-
-    def copy_initializer_file
-      unless options[:orm]
-        raise MissingORMError, <<-ERROR.strip_heredoc
-        An ORM must be set to install Devise in your application.
-        Be sure to have an ORM like Active Record or Mongoid loaded in your
-        app or configure your own at `config/application.rb`.
-          config.generators do |g|
-            g.orm :your_orm_gem
-          end
-        ERROR
+      def self.source_root
+        @source_root ||= File.join(File.dirname(__FILE__), 'templates')
       end
 
-      copy_file "initializer.rb", "config/initializers/send_in_blue.rb"
-    end
-  end
+      def copy_initializer_file
+        unless options[:orm]
+          raise MissingORMError, <<-ERROR.strip_heredoc
+          An ORM must be set to install Devise in your application.
+          Be sure to have an ORM like Active Record or Mongoid loaded in your
+          app or configure your own at `config/application.rb`.
+            config.generators do |g|
+              g.orm :your_orm_gem
+            end
+          ERROR
+        end
 
-  def create_email_templates_model
-    generate "model", "SendInBlueEmailTemplates", "sib_template_id:integer event:integer"
+        copy_file "initializer.rb", "config/initializers/send_in_blue.rb"
+      end
+
+      def create_email_templates_model
+        generate "model", "SendInBlueEmailTemplates", "sib_template_id:integer event:integer"
+      end
+    end
   end
 end
