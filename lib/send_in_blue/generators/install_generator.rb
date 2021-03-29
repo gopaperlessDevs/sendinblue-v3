421e6dc0bfb2117c050f21c631871194aab119c7
Ethaning <ethanjcarter@hotmail.co.uk>
2021-03-29 16:18:34 +0100

    Remove orm restraint in generator

--- lib/send_in_blue/generators/install_generator.rb
+++ lib/send_in_blue/generators/install_generator.rb
@@ -6,23 +6,23 @@ module SendInBlue
 
     class InstallGenerator < Rails::Generators::Base
       desc "Setup Send In Blue integration"
-      class_option :orm, required: true
+      # class_option :orm, required: true
 
       def self.source_root
         @source_root ||= File.join(File.dirname(__FILE__), 'templates')
       end
 
       def copy_initializer_file
-        unless options[:orm]
-          raise MissingORMError, <<-ERROR.strip_heredoc
-          An ORM must be set to install Devise in your application.
-          Be sure to have an ORM like Active Record or Mongoid loaded in your
-          app or configure your own at `config/application.rb`.
-            config.generators do |g|
-              g.orm :your_orm_gem
-            end
-          ERROR
-        end
+        # unless options[:orm]
+        #   raise MissingORMError, <<-ERROR.strip_heredoc
+        #   An ORM must be set to install Devise in your application.
+        #   Be sure to have an ORM like Active Record or Mongoid loaded in your
+        #   app or configure your own at `config/application.rb`.
+        #     config.generators do |g|
+        #       g.orm :your_orm_gem
+        #     end
+        #   ERROR
+        # end
 
         copy_file "initializer.rb", "config/initializers/send_in_blue.rb"
       end
