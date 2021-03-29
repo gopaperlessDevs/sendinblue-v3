664c8bf719bf93ce77f3adcb13d855a62a72efb2
Ethaning <ethanjcarter@hotmail.co.uk>
2021-03-29 16:14:35 +0100

    Require generator modules in root module

--- lib/send_in_blue.rb
+++ lib/send_in_blue.rb
@@ -2,13 +2,20 @@
 
 require_relative "send_in_blue/version"
 
+# dependencies
 require 'sib-api-v3-sdk'
 require 'active_support/concern'
 require 'sidekiq'
 
+# app code
 require 'send_in_blue/contact'
 require 'send_in_blue/workers/contact_worker'
 require 'send_in_blue/workers/event_email_worker'
+
+# generators
+require 'send_in_blue/generators/install_generator'
+require 'send_in_blue/generators/templates/initializer'
+
 
 module SendInBlue
   class Error < StandardError; end
