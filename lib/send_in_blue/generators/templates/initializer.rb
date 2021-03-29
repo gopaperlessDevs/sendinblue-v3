19948a074d19ac29502f381aba1494af68266801
Ethaning <ethanjcarter@hotmail.co.uk>
2021-03-29 13:58:29 +0100

    Add install generator

---
+++ lib/send_in_blue/generators/templates/initializer.rb
@@ -0,0 +1,12 @@
+# setup authorization
+SibApiV3Sdk.configure do |config|
+  # Configure API key authorization: api-key
+  config.api_key['api-key'] = 'YOUR API KEY'
+  # Uncomment the following line to set a prefix for the API key, e.g. 'Bearer' (defaults to nil)
+  #config.api_key_prefix['api-key'] = 'Bearer'
+
+  # Configure API key authorization: partner-key
+  config.api_key['partner-key'] = 'YOUR API KEY'
+  # Uncomment the following line to set a prefix for the API key, e.g. 'Bearer' (defaults to nil)
+  #config.api_key_prefix['partner-key'] = 'Bearer'
+end
