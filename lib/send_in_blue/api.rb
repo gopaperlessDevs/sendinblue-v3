e2d800bc2c7fa2c3840d72f40ae2c47e87064256
Ethaning <ethanjcarter@hotmail.co.uk>
2021-03-31 13:24:05 +0100

    Update interface for SendInBlue API calls

--- lib/send_in_blue/api.rb
+++ lib/send_in_blue/api.rb
@@ -1,12 +1,42 @@
 module SendInBlue
   class Api
     class << self
+      def create_contact(contact)
+        contact_data = SibApiV3Sdk::CreateContact.new email: contact.email, attributes: contact.send_in_blue_attributes
+
+        result = contacts_api.create_contact(contact_data)
+
+        contact.update_send_in_blue_id!(JSON.parse(res)["id"])
+        result
+      end
+
+      def update_contact(contact)
+        # if contact doesnt have sendinblue id, then create contact
+        return create_contact(contact) unless contact.contact_send_in_blue_id
+        return false unless contact.consents_to_send_in_blue_email?
+
+        contacts_api.update_contact(
+          contact.contact_send_in_blue_id,
+          SibApiV3Sdk::UpdateContact.new(contact.send_in_blue_attributes)
+        )
+      end
+
+      def delete_contact(contact)
+        contacts_api.delete_contact(contact.contact_send_in_blue_id)
+      end
+
       def get_attributes
         contacts_api.get_attributes
       end
 
-      def delete_attribute(category, attribute_name)
-        attributes_api.delete_attribute(category, attribute_name)
+      def create_attribute(attr_name, type, category = "normal")
+        attr_properties = SibApiV3Sdk::CreateAttribute.new(type: type)
+
+        attributes_api.create_attribute(category, attr_name, attr_properties)
+      end
+
+      def delete_attribute(attr_name, category = "normal")
+        attributes_api.delete_attribute(category, attr_name)
       end
 
       private
--- lib/send_in_blue/workers/contact_worker.rb
+++ lib/send_in_blue/workers/contact_worker.rb
@@ -20,23 +20,14 @@ class SendInBlue::ContactWorker
   end
 
   def create
-    res = api_instance.create_contact(SibApiV3Sdk::CreateContact.new(contact.send_in_blue_attributes))
-
-    contact.update_send_in_blue_id!(JSON.parse(res)["id"])
+    res = SendInBlue::API.create_contact(contact)
   end
 
   def update
-    # if contact doesnt have sendinblue id, then create contact
-    return create unless contact.contact_send_in_blue_id
-    return false unless contact.consents_to_send_in_blue_email?
-
-    api_instance.update_contact(
-      contact.contact_send_in_blue_id,
-      SibApiV3Sdk::UpdateContact.new(contact.send_in_blue_attributes)
-    )
+    res = SendInBlue::API.update_contact(contact)
   end
 
   def delete
-    api_instance.delete_contact(contact.contact_send_in_blue_id)
+    res = SendInBlue::API.delete_contact(contact)
   end
 end
